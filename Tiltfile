15ec87840edf16a2e454d4148da645fa85f50306
Amim Knabben <amim.knabben@gmail.com>
2019-08-15 14:04:48 -0400

    Initial tilt front usage

--- Tiltfile
+++ Tiltfile
@@ -1,3 +1,9 @@
-k8s_yaml('deploy.yaml')
+#k8s_yaml('deploy.yaml')
 
-k8s_resource('sawtooth-rest-api', port_forwards=8008)
+docker_build("frontend", "./front/",
+  live_update=[
+    fall_back_on("front/package.json"),
+    sync("./front/src", "/app/src")
+  ])
+k8s_yaml("front/deploy.yaml")
+k8s_resource("frontend", port_forwards=3000)
---
+++ front/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import styled from 'styled-components'
+
+import './App.css';
+
+const Header = styled.div`
+  background-color: red
+`
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Header>blxxxxxxxa</Header>
+      </header>
+    </div>
+  );
+}
+
+export default App;
